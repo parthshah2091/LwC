6982945adecc7cf53dd22bd796773e9b7b6e9a68
parthshah2091 <parthshah2091@gmail.com>
2022-01-18 16:43:07 +1100

    Done Commit

--- force-app/ToDoManager/force-app/main/default/lwc/childComponent/childComponent.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement ,api } from 'lwc';
-
-export default class ChildComponent extends LightningElement {
-    @api inputItem;
-    fireSelectEvent(){
-        const inputItem = new CustomEvent("inputitemselect",{detail:{value:this.inputItem}});
-        this.dispatchEvent(inputItem);
-    }
-}
